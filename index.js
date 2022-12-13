929f879c5be61f9cfeed852337aade803f0d525d
StyxSlayer <83481046+StyxSlayer@users.noreply.github.com>
2022-12-12 18:02:19 -0800

    init build

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO1aIe_fTZB6duj8YIRyYcLTINlcP196w",
+  authDomain: "escouting-7b4e0.firebaseapp.com",
+  databaseURL: "https://escouting-7b4e0-default-rtdb.firebaseio.com",
+  projectId: "escouting-7b4e0",
+  storageBucket: "escouting-7b4e0.appspot.com",
+  messagingSenderId: "377179821867",
+  appId: "1:377179821867:web:cedab35ab708c12986976e",
+  measurementId: "G-8VWYRF9QY6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
